9c3723c9c0a8565ac3e401814f4b0cc70de43e86
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2026-08-17 12:53:46 +0330

    Add Structure

--- alm/models.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class User(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    username: str = Field(index=True)
-    email: str
-    password : str
-    age: int
